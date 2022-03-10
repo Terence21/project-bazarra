b7d813faa6bea740450e61eabd29ff177ede362e
Terence Peterson <tee@Terences-MBP.fios-router.home>
2022-03-10 11:43:22 -0500

    configured mongodb connection template using dotenv for future changes

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
 const express = require('express')
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
+
+var router = express.Router()
 const app = express()
-const port = 3000
+const port = process.env.PORT
+
+
+process.env.TEST // "TEST"
+process.env.BAZARRA // "BAZARRA"
+
+
+
+app.listen(port, () => {
+    console.log(`Project ${process.env.BAZARRA} listening on port ${port}`)
+})
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello From Bazarra')
 })
-  
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+
+// Replace the uri string with BAZ MongoDB deployment's connection env variable.
+const uri = process.env.MONGODB;
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+      await client.connect();
+    } catch{
+        console.log('client database cluster connection failed')
+    } 
+    finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.error)
+
+async function logDatabaseConnections(client){
+    databaseConnections = await client.db().admin().logDatabaseConnections();
+ 
+    console.log("Databases:");
+    databaseConnections.databases.forEach(db => console.log(` - ${db.name}`));
+};
