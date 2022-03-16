24efca66b8f755778ffa3efd5f3a53bbd5d4cadb
Terence Peterson <tee@Terences-MBP.fios-router.home>
2022-03-16 06:20:06 -0400

    BAZ-82 valid mongodb connection

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const { MongoClient } = require("mongodb");
+const {MongoClient} = require("mongodb");
 require('dotenv').config();
 
 var router = express.Router()
 const app = express()
 const port = process.env.PORT
 
-process.env.TEST // "TEST"
-process.env.BAZARRA // "BAZARRA"
+process.env.TEST
+process.env.BAZARRA
 
 app.listen(port, () => {
     console.log(`Project ${process.env.BAZARRA} listening on port ${port}`)
@@ -17,26 +17,27 @@ app.get('/', (req, res) => {
     res.send('Hello From Bazarra')
 })
 
-// Replace the uri string with BAZ MongoDB deployment's connection env variable.
 const uri = process.env.MONGODB;
 const client = new MongoClient(uri);
 
 async function run() {
     try {
-      await client.connect();
-    } catch{
+        await client.connect();
+        await logDatabaseConnections(client)
+        console.log("Connection Successful")
+    } catch (e) {
         console.log('client database cluster connection failed')
-    } 
-    finally {
-        // Ensures that the client will close when you finish/error
+        console.log(e.message)
+    } finally {
         await client.close();
     }
 }
+
 run().catch(console.error)
 
-async function logDatabaseConnections(client){
-    databaseConnections = await client.db().admin().logDatabaseConnections();
- 
+async function logDatabaseConnections(client) {
+    databaseConnections = await client.db().admin().listDatabases()
+
     console.log("Databases:");
     databaseConnections.databases.forEach(db => console.log(` - ${db.name}`));
-};
+}
